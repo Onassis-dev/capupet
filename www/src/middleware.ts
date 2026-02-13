b4b14e733acedf1f366402e804df1b0a8d005e9c
Onassis <onassis.dev@gmail.com>
2026-02-12 19:18:57 -0800

    fix: improve middleware host validation and add debug logs
    - Updated middleware to allow subdomains ending with ".capu.pet".
    - Added console logs in index and ID pages for debugging purposes.

--- www/src/middleware.ts
+++ www/src/middleware.ts
@@ -10,12 +10,13 @@ export const onRequest = defineMiddleware((context, next) => {
   if (/\/ugc-(404|500)\/?$/.test(pathname)) return next(); // I don't like this
   if (pathname.startsWith("/ugc/")) return next("/404");
   if (!host) return next();
-  if (!host?.endsWith("capu.pet")) return next();
+  if (!host?.endsWith(".capu.pet")) return next();
 
   const parts = host.split(".");
   const subdomain = parts.length > 2 ? parts.at(-3) : null;
 
   if (subdomain) {
+    console.log("REDIRECTING: ", `/ugc/${subdomain}${pathname}`);
     return next(`/ugc/${subdomain}${pathname}`);
   }
 
